235fe857289716c875187afd5124e88497db2104
Musa Almatri <almatri93@gmail.com>
2022-05-22 23:17:09 +0300

    make test fail

--- Numero/Converter.swift
+++ Numero/Converter.swift
@@ -45,7 +45,7 @@ class Converter {
        (9, "IX"),
        (5, "V"),
        (4, "IV"),
-       (1, "I")]
+       (1, "U")]
     
     for item in numberSymbols {
       while localNumber >= item.number {
