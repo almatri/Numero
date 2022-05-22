1aeb7bb2b7405f9438e5b7ad6a837f2210e639e3
Musa Almatri <almatri93@gmail.com>
2022-05-22 23:15:25 +0300

    update unit test

--- NumeroTests/ConverterTests.swift
+++ NumeroTests/ConverterTests.swift
@@ -46,7 +46,7 @@ class ConverterTests: XCTestCase {
   
   func testConversionForOne() {
     let result = converter.convert(1)
-    XCTAssertEqual(result, "I", "Conversion for 1 is incorrect, fix it")
+    XCTAssertEqual(result, "I", "Conversion for 1 is incorrect")
   }
   
   func testConversionForTwo() {
