4f09a2ea5d18e2b2ebf850a476ea7f2c754eaabc
Musa Almatri <almatri93@gmail.com>
2022-05-23 18:28:30 +0300

    update ci.sh

--- ci.sh
+++ ci.sh
@@ -1,3 +1,3 @@
 set -e
-xcodebuild test -project 'Numero.xcodeproj' -scheme 'Numero' -destination 'platform=iOS Simulator,name=iPhone 8' | xcpretty
-xcodebuild build -project 'Numero.xcodeproj' -scheme 'Numero' -destination 'generic/platform=iOS' -configuration Release CODE_SIGNING_ALLOWED=NO | xcpretty
+xcodebuild test -project 'Numero.xcodeproj' -scheme 'Numero' -destination 'platform=iOS Simulator,name=iPhone 8'
+xcodebuild build -project 'Numero.xcodeproj' -scheme 'Numero' -destination 'generic/platform=iOS' -configuration Release CODE_SIGNING_ALLOWED=NO
